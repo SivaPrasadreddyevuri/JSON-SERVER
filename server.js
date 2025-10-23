d8118b5b19b142142c924f52ef5c8c0759704cd2
SivaPrasad Reddy Devloper <evurisivaprasadreddy3@gmail.com>
2025-10-23 16:31:03 +0530

    changes

--- server.js
+++ server.js
@@ -1,12 +1,55 @@
-const jsonServer = require('json-server')
+// const jsonServer = require('json-server')
 
-const server = jsonServer.create()
+// const server = jsonServer.create()
 
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+// const router = jsonServer.router('db.json')
+// const middlewares = jsonServer.defaults()
  
-server.use(middlewares)
-server.use('/api', router)
-server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+// server.use(middlewares)
+// server.use('/api', router)
+// server.listen(process.env.PORT || 5000, () => {
+//   console.log('JSON Server is running')
+// })
+
+
+function abc(){
+  console.log(abc.xyz)
+}
+abc()
+abc.xyz=400
+abc.xyz=200
+abc()
+
+
+
+console.log( typeof typeof typeof 100)
+
+
+
+
+
+
+
+
+// let http=require("http")
+
+// let server1 =http.createServer((req,res)=>{
+//     res.write("heloo world")
+//     res.end()
+// })
+
+
+// server1.listen(145,()=>{
+//      console.log("server created")
+// })
+// let http = require("http");
+
+// let server1 = http.createServer((req, res) => {
+//     res.write("hello world");
+//     res.end();
+// });
+
+// // Listen on port 1456
+// server1.listen(1456, () => {
+//     console.log("Server is running on port 1456");
+// });
